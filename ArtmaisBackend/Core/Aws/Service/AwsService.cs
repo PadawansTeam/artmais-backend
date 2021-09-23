8844e79135080988265c50cd24b64c03cd929222
João Victor Teixeira De Oliveira <joliveira@acesso.com>
2021-09-23 00:30:02 -0300

    WIP

--- ArtmaisBackend/Core/Aws/Service/AwsService.cs
+++ ArtmaisBackend/Core/Aws/Service/AwsService.cs
@@ -1,35 +1,31 @@
 ﻿using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
-using Amazon.S3.Transfer;
 using ArtmaisBackend.Core.Aws.Dto;
 using ArtmaisBackend.Core.Aws.Interface;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using System;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace ArtmaisBackend.Core.Aws.Service
 {
     public class AwsService : IAwsService
     {
-        public AwsService(IMapper mapper, IAmazonS3 amazonS3)
+        public AwsService(IAmazonS3 amazonS3)
         {
-            this._mapper = mapper;
-            this._amazonS3 = amazonS3;
+            _amazonS3Client = new AmazonS3Client(bucketRegion);
         }
 
         private const string bucketName = "bucket-artmais";
         private static readonly string objectKey = $"ARTMAIS{DateTime.Today.ToString("yyyyMMdd")}{new Random((int)DateTime.Now.Ticks).Next().ToString("D14")}";
         private const string filePath = "profile-pictures/";
-        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USWest2;
-        private readonly IAmazonS3 _amazonS3;
-        private readonly IMapper _mapper;
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+        private readonly IAmazonS3 _amazonS3Client;
 
         public async Task<AwsDto?> UploadObjectAsync(IFormFile? file, long userId)
         {
-            var putRequest = new PutObjectRequest()
+            var putRequest = new PutObjectRequest
             {
                 BucketName = bucketName,
                 Key = filePath + userId + "/" + objectKey + ".jpg",
@@ -38,8 +34,8 @@ namespace ArtmaisBackend.Core.Aws.Service
                 CannedACL = S3CannedACL.PublicRead,
             };
 
-            var result = await this._amazonS3.PutObjectAsync(putRequest);
-            
+            var result = await _amazonS3Client.PutObjectAsync(putRequest);
+
             return new AwsDto
             {
                 Picture = result.RequestCharged
