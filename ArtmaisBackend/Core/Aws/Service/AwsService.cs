1413c0755c95699ebc5b55c7db3dae3ee8f09e19
João Victor Teixeira De Oliveira <joliveira@acesso.com>
2021-10-02 13:57:16 -0300

    Refactor aws

--- ArtmaisBackend/Core/Aws/Service/AwsService.cs
+++ ArtmaisBackend/Core/Aws/Service/AwsService.cs
@@ -20,47 +20,41 @@ namespace ArtmaisBackend.Core.Aws.Service
         }
 
         private readonly IUserRepository _userRepository;
-        private const string bucketName = "bucket-artmais";
-        private const string filePath = "profile-pictures/";
-        private static readonly string objectKey = $"{DateTime.Today.ToString("yyyyMMdd")}{new Random((int)DateTime.Now.Ticks).Next().ToString("D14")}";
-        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
-        public static S3CannedACL fileCannedACL = S3CannedACL.PublicRead;
-        private static IAmazonS3 client;
+        private static readonly IAmazonS3 client = new AmazonS3Client(RegionEndpoint.USEast1);
         private readonly IMapper _mapper;
 
         public async Task<AwsDto?> UploadObjectAsync(IFormFile? file, long userId)
-        {
-            client = new AmazonS3Client(bucketRegion);
+        {       
             var response = await this.WritingAnObjectAsync(file, userId);
             return new AwsDto(response);
         }
 
         public async Task<string> WritingAnObjectAsync(IFormFile? file, long userId)
         {
+            if (file is null) throw new ArgumentNullException();
+
             try
             {
-                if (file is null)
-                    throw new ArgumentNullException();
-
                 var fs = file.OpenReadStream();
                 var extension = file.FileName.Split(".");
-                var keyName = filePath + userId + "/" + objectKey + "." + extension[1];
-                var putRequest1 = new PutObjectRequest
+                var keyName = $"{AwsConfiguration.FilePath}{userId}/{AwsConfiguration.ObjectKey}.{extension[1]}";
+
+                var putRequest = new PutObjectRequest
                 {
-                    BucketName = bucketName,
+                    BucketName = AwsConfiguration.BucketName,
                     Key = keyName,
                     InputStream = fs,
                     ContentType = file.ContentType,
-                    CannedACL = fileCannedACL
+                    CannedACL = S3CannedACL.PublicRead
                 };
 
-                PutObjectResponse response = await client.PutObjectAsync(putRequest1);
+                await client.PutObjectAsync(putRequest);
 
-                var urlAws = string.Format("http://{0}.s3.amazonaws.com/{1}", bucketName, keyName);
-                var request = new AwsDto(urlAws);
+                var urlAws = string.Format("http://{0}.s3.amazonaws.com/{1}", AwsConfiguration.BucketName, keyName);
+                var dto = new AwsDto(urlAws);
+               
                 var userInfo = this._userRepository.GetUserById(userId);
-                var updateUserPicture = this._userRepository.UpdateUserPicture(urlAws);
-                this._mapper.Map(request, userInfo);
+                this._mapper.Map(dto, userInfo);
                 this._userRepository.Update(userInfo);
 
                 return urlAws;
@@ -68,14 +62,6 @@ namespace ArtmaisBackend.Core.Aws.Service
             catch (AmazonS3Exception e)
             {
                 throw new AmazonS3Exception(e.Message);
-            }
-            catch (ArgumentNullException e)
-            {
-                throw new ArgumentNullException(e.Message);
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
             }
         }
     }
