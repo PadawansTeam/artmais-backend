755eb8d1f9bc83d8a99da0e567a9b4ffaee44a88
Juliana Santiago <50492822+juliana-santiago@users.noreply.github.com>
2021-10-17 20:17:31 -0300

    Add method to find out if post has already been liked

--- ArtmaisBackend/Core/Publications/Service/PublicationService.cs
+++ ArtmaisBackend/Core/Publications/Service/PublicationService.cs
@@ -86,5 +86,18 @@ namespace ArtmaisBackend.Core.Publications.Service
 
             return true;
         }
+
+        public bool GetIsLikedPublication(int? publicationId, long userId)
+        {
+            if (publicationId is null)
+                throw new ArgumentNullException();
+
+            var isLiked = _likeRepository.GetLikeByPublicationIdAndUserId(publicationId, userId);
+
+            if (isLiked is null)
+               return false;
+
+            return true;
+        }
     }
 }
