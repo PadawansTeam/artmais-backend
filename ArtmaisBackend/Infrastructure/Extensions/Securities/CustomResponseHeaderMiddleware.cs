c637ab204f402d86119e5cae4d3ab6c15be97955
Juliana Santiago <50492822+juliana-santiago@users.noreply.github.com>
2021-11-07 19:01:44 -0300

    Add http headers

---
+++ ArtmaisBackend/Infrastructure/Extensions/Securities/CustomResponseHeaderMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace ArtmaisBackend.Infrastructure.Extensions.Securities
+{
+    [ExcludeFromCodeCoverage]
+    public class CustomResponseHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomResponseHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.OnStarting(state =>
+            {
+                var httpContext = (HttpContext)state;
+                httpContext.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                httpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                httpContext.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
+                httpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                return Task.CompletedTask;
+            }, context);
+
+            await _next(context);
+        }
+    }
+}
