1777c48d3022d6e532c11807702e412bf1ce7ea0
joao.teixeira <joao.teixeira@checkok.com.br>
2021-11-29 17:09:56 -0300

    Add GetAsync

--- ArtmaisBackend/Core/Payments/Interface/IMercadoPagoPaymentClient.cs
+++ ArtmaisBackend/Core/Payments/Interface/IMercadoPagoPaymentClient.cs
@@ -10,5 +10,6 @@ namespace ArtmaisBackend.Core.Payments.Interface
     public interface IMercadoPagoPaymentClient
     {
         public Task<MercadoPago.Resource.Payment.Payment> CreateAsync(PaymentCreateRequest request, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+        public Task<MercadoPago.Resource.Payment.Payment> GetAsync(long id, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
     }
 }
