e27364c8866ee0e7656256957952f73b78d448ef
joao.teixeira <joao.teixeira@checkok.com.br>
2021-12-08 15:11:27 -0300

    Add PasswordRecoveryDefaults

---
+++ ArtmaisBackend/Core/PasswordRecovery/PasswordRecoveryDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtmaisBackend.Core.PasswordRecovery
+{
+    public static class PasswordRecoveryDefaults
+    {
+        public static readonly string USER_NOT_FOUND = "E-mail não cadastrado";
+        public static readonly string VERIFICATION_CODE_SUBJECT = "Código de verificação";
+        public static readonly string INVALID_VERIFICATION_CODE = "Código de verificação expirado e/ou inválido";
+        public static readonly string INVALID_PASSWORD = "Senhas inválidas e/ou diferentes";
+    }
+}
