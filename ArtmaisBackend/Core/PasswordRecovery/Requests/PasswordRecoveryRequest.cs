43be71a8b7a3495f7f766b7f1dad03bd84912f9a
joao.teixeira <joao.teixeira@checkok.com.br>
2021-12-08 15:12:18 -0300

    Add PasswordRecoveryRequest

---
+++ ArtmaisBackend/Core/PasswordRecovery/Requests/PasswordRecoveryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtmaisBackend.Core.PasswordRecovery.Requests
+{
+    public class PasswordRecoveryRequest
+    {
+        public long UserId { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirmation { get; set; }
+    }
+}
