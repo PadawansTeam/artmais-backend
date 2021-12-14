94bd88a5b095984e969ea231fc7fa6e9bb6c1b61
joao.teixeira <joao.teixeira@checkok.com.br>
2021-12-14 18:15:01 -0300

    Add AnswerRequest and AnswerDto

---
+++ ArtmaisBackend/Core/Answer/Requests/AnswerRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ArtmaisBackend.Core.Answer.Requests
+{
+    public class AnswerRequest
+    {
+        public string Description { get; set; }
+        public int CommentId { get; set; }
+    }
+}
