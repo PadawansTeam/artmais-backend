e0124087842550551499647a5ff41da65b62b9d9
joao.teixeira <joao.teixeira@checkok.com.br>
2021-12-14 18:15:18 -0300

    Add Answer

---
+++ ArtmaisBackend/Core/Entities/Answer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtmaisBackend.Core.Entities
+{
+    [Table("resposta")]
+    public class Answer
+    {
+        [Column("idresposta")]
+        public long AnswerID { get; set; }
+
+        [Column("idcomentario")]
+        public int CommentID { get; set; }
+
+        [Column("CommentID")]
+        public Comment Comment { get; set; }
+
+        [Column("resposta")]
+        public string Description { get; set; }
+
+        [Column("datahora")]
+        public DateTime DateTime { get; set; }
+
+        [Column("idusuario")]
+        public long UserID { get; set; }
+
+        [Column("UserID")]
+        public User User { get; set; }
+    }
+}
